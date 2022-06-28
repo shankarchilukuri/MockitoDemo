cf48b2c0bbcb0002329b7fb2ee8683a787b592a7
ch.b.shankar1@gmail.com <ch.b.shankar1@gmail.com>
2022-06-28 12:33:56 +0530

    New class

---
+++ Java/src/Class.java
@@ -0,0 +1,16 @@
+
+public class Class {
+
+	public static void main(String[] args) {
+
+		int[][] arr = { { 1, 5 }, { 5, 5 } };
+		for (int i = 0; i < 5; i++) {
+			for (int j = 0; j < 5; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+			System.out.println();
+		}
+	}
+
+}
